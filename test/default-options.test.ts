aa46e3d64d2dd3cef0b7ea83242edc96fa5e3c03
Patrick Cate <patrickcate@users.noreply.github.com>
2025-04-14 22:44:00 -0400

    test: remove un-necessary awaits

--- test/default-options.test.ts
+++ test/default-options.test.ts
@@ -15,22 +15,20 @@ describe('default option', async () => {
     const nuxtUswdsOptions = await page
       .locator('.test-nuxt-uswds-options')
       .textContent();
-    await expect(nuxtUswdsOptions).toContain(
-      `"autoImportBaseComponents": true`,
-    );
-    await expect(nuxtUswdsOptions).toContain(`"autoImportComponents": true`);
-    await expect(nuxtUswdsOptions).toContain(`"baseComponentPrefix": ""`);
-    await expect(nuxtUswdsOptions).toContain(`"componentPrefix": ""`);
+    expect(nuxtUswdsOptions).toContain(`"autoImportBaseComponents": true`);
+    expect(nuxtUswdsOptions).toContain(`"autoImportComponents": true`);
+    expect(nuxtUswdsOptions).toContain(`"baseComponentPrefix": ""`);
+    expect(nuxtUswdsOptions).toContain(`"componentPrefix": ""`);
 
     const vueUswdsOptions = await page
       .locator('.test-vue-uswds-options')
       .textContent();
-    await expect(vueUswdsOptions).toContain(`{}`);
+    expect(vueUswdsOptions).toContain(`{}`);
 
     const usaTag = await page
       .locator('.test-usa-component span.usa-tag')
       .textContent();
-    await expect(usaTag).toContain('Test UsaTag');
+    expect(usaTag).toContain('Test UsaTag');
 
     const baseLink = await page.locator('.test-base-component a').textContent();
     await expect(baseLink).toContain('Test BaseLink');
